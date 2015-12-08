f5934891667f0ec9d05b90d0e2078486a0f27efa
Jerry <lijie@1000phone.com>
2015-12-08 21:36:35 +0800

    add UISegmentControl at project view controller

--- CodingPlatform/ProjectViewController.m
+++ CodingPlatform/ProjectViewController.m
@@ -8,6 +8,8 @@
 
 #import "ProjectViewController.h"
 
+#define IndicateTag 100
+
 @interface ProjectViewController ()
 
 @end
@@ -20,6 +22,8 @@
     //添加导航条上的左右item
     [self addItemWithImage:@"search_Nav" target:self action:@selector(searchHandler) isLeft:YES];
     [self addItemWithImage:@"addBtn_Nav" target:self action:@selector(addHandler) isLeft:NO];
+    //创建分段控制器
+    [self createSegmentControl];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -73,6 +77,38 @@
 - (void)addHandler
 {
     NSLog(@"点击添加按钮");
+}
+
+- (void)createSegmentControl
+{
+    UISegmentedControl *sc = [[UISegmentedControl alloc] initWithItems:@[@"全部项目",@"我参与的",@"我创建的"]];
+    sc.selectedSegmentIndex = 0;
+    sc.frame = CGRectMake(0, 0, VIEW_WIDTH, 40);
+    sc.tintColor = [UIColor clearColor];
+    [sc setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor blackColor],NSFontAttributeName:[UIFont systemFontOfSize:15]} forState:UIControlStateNormal];
+    [sc setTitleTextAttributes:@{NSForegroundColorAttributeName:LightGreenColor,NSFontAttributeName:[UIFont systemFontOfSize:15]} forState:UIControlStateSelected];
+    [sc addTarget:self action:@selector(changePlate:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:sc];
+    //创建水平线
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 43.5, VIEW_WIDTH, 0.5)];
+    label.backgroundColor = [UIColor grayColor];
+    [self.view addSubview:label];
+    //创建用于指示选中状态的label
+    CGFloat width = VIEW_WIDTH/sc.numberOfSegments;
+    UILabel *indicateLabel = [[UILabel alloc] initWithFrame:CGRectMake(width*sc.selectedSegmentIndex, 42, width, 2)];
+    indicateLabel.backgroundColor = LightGreenColor;
+    [self.view addSubview:indicateLabel];
+    indicateLabel.tag = IndicateTag;
+}
+
+- (void)changePlate:(UISegmentedControl *)sc
+{
+    [UIView animateWithDuration:0.2 animations:^{
+        UIView *view = [self.view viewWithTag:IndicateTag];
+        CGRect frame = view.frame;
+        frame.origin.x = sc.selectedSegmentIndex*frame.size.width;
+        view.frame = frame;
+    }];
 }
 
 @end
